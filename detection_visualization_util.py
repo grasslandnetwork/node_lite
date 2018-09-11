6b97a7530125ae6dd714196eeb8e10be888836f9
David Thompson <00hello@users.noreply.github.com>
2018-09-11 11:26:51 -0400

    Now using Serverless Lambda AWS for object detections. Beginning of rudimentary 3D camera calibration using lnglat_homography.py. Threads, multiprocess pools, and queues. Tracks multiple objects using OpenCv. Entry point now 'multi_object_tracking.py'

--- detection_visualization_util.py
+++ detection_visualization_util.py
@@ -1,4 +1,41 @@
 # Taken from https://github.com/tensorflow/models/blob/master/research/object_detection/utils/visualization_utils.py
+import collections
+import numpy as np
+import PIL.Image as Image
+import PIL.ImageColor as ImageColor
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+
+
+STANDARD_COLORS = [
+    'AliceBlue', 'Chartreuse', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque',
+    'BlanchedAlmond', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'AntiqueWhite',
+    'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan',
+    'DarkCyan', 'DarkGoldenRod', 'DarkGrey', 'DarkKhaki', 'DarkOrange',
+    'DarkOrchid', 'DarkSalmon', 'DarkSeaGreen', 'DarkTurquoise', 'DarkViolet',
+    'DeepPink', 'DeepSkyBlue', 'DodgerBlue', 'FireBrick', 'FloralWhite',
+    'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod',
+    'Salmon', 'Tan', 'HoneyDew', 'HotPink', 'IndianRed', 'Ivory', 'Khaki',
+    'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue',
+    'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGrey',
+    'LightGreen', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue',
+    'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime',
+    'LimeGreen', 'Linen', 'Magenta', 'MediumAquaMarine', 'MediumOrchid',
+    'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen',
+    'MediumTurquoise', 'MediumVioletRed', 'MintCream', 'MistyRose', 'Moccasin',
+    'NavajoWhite', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed',
+    'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed',
+    'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple',
+    'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Green', 'SandyBrown',
+    'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue',
+    'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'GreenYellow',
+    'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White',
+    'WhiteSmoke', 'Yellow', 'YellowGreen'
+]
+
+category_index = {1: {'name': 'person', 'id': 1}, 2: {'name': 'bicycle', 'id': 2}, 3: {'name': 'car', 'id': 3}, 4: {'name': 'motorcycle', 'id': 4}, 5: {'name': 'airplane', 'id': 5}, 6: {'name': 'bus', 'id': 6}, 7: {'name': 'train', 'id': 7}, 8: {'name': 'truck', 'id': 8}, 9: {'name': 'boat', 'id': 9}, 10: {'name': 'traffic light', 'id': 10}, 11: {'name': 'fire hydrant', 'id': 11}, 13: {'name': 'stop sign', 'id': 13}, 14: {'name': 'parking meter', 'id': 14}, 15: {'name': 'bench', 'id': 15}, 16: {'name': 'bird', 'id': 16}, 17: {'name': 'cat', 'id': 17}, 18: {'name': 'dog', 'id': 18}, 19: {'name': 'horse', 'id': 19}, 20: {'name': 'sheep', 'id': 20}, 21: {'name': 'cow', 'id': 21}, 22: {'name': 'elephant', 'id': 22}, 23: {'name': 'bear', 'id': 23}, 24: {'name': 'zebra', 'id': 24}, 25: {'name': 'giraffe', 'id': 25}, 27: {'name': 'backpack', 'id': 27}, 28: {'name': 'umbrella', 'id': 28}, 31: {'name': 'handbag', 'id': 31}, 32: {'name': 'tie', 'id': 32}, 33: {'name': 'suitcase', 'id': 33}, 34: {'name': 'frisbee', 'id': 34}, 35: {'name': 'skis', 'id': 35}, 36: {'name': 'snowboard', 'id': 36}, 37: {'name': 'sports ball', 'id': 37}, 38: {'name': 'kite', 'id': 38}, 39: {'name': 'baseball bat', 'id': 39}, 40: {'name': 'baseball glove', 'id': 40}, 41: {'name': 'skateboard', 'id': 41}, 42: {'name': 'surfboard', 'id': 42}, 43: {'name': 'tennis racket', 'id': 43}, 44: {'name': 'bottle', 'id': 44}, 46: {'name': 'wine glass', 'id': 46}, 47: {'name': 'cup', 'id': 47}, 48: {'name': 'fork', 'id': 48}, 49: {'name': 'knife', 'id': 49}, 50: {'name': 'spoon', 'id': 50}, 51: {'name': 'bowl', 'id': 51}, 52: {'name': 'banana', 'id': 52}, 53: {'name': 'apple', 'id': 53}, 54: {'name': 'sandwich', 'id': 54}, 55: {'name': 'orange', 'id': 55}, 56: {'name': 'broccoli', 'id': 56}, 57: {'name': 'carrot', 'id': 57}, 58: {'name': 'hot dog', 'id': 58}, 59: {'name': 'pizza', 'id': 59}, 60: {'name': 'donut', 'id': 60}, 61: {'name': 'cake', 'id': 61}, 62: {'name': 'chair', 'id': 62}, 63: {'name': 'couch', 'id': 63}, 64: {'name': 'potted plant', 'id': 64}, 65: {'name': 'bed', 'id': 65}, 67: {'name': 'dining table', 'id': 67}, 70: {'name': 'toilet', 'id': 70}, 72: {'name': 'tv', 'id': 72}, 73: {'name': 'laptop', 'id': 73}, 74: {'name': 'mouse', 'id': 74}, 75: {'name': 'remote', 'id': 75}, 76: {'name': 'keyboard', 'id': 76}, 77: {'name': 'cell phone', 'id': 77}, 78: {'name': 'microwave', 'id': 78}, 79: {'name': 'oven', 'id': 79}, 80: {'name': 'toaster', 'id': 80}, 81: {'name': 'sink', 'id': 81}, 82: {'name': 'refrigerator', 'id': 82}, 84: {'name': 'book', 'id': 84}, 85: {'name': 'clock', 'id': 85}, 86: {'name': 'vase', 'id': 86}, 87: {'name': 'scissors', 'id': 87}, 88: {'name': 'teddy bear', 'id': 88}, 89: {'name': 'hair drier', 'id': 89}, 90: {'name': 'toothbrush', 'id': 90}}
+
+
 
 def draw_mask_on_image_array(
         image,
@@ -90,14 +127,77 @@ def draw_bounding_box_on_image_array(
                                use_normalized_coordinates)
     np.copyto(image, np.array(image_pil))
 
-  
+
+def draw_bounding_box_on_image(
+        image,
+        ymin,
+        xmin,
+        ymax,
+        xmax,
+        color='red',
+        thickness=4,
+        display_str_list=(),
+        use_normalized_coordinates=True):
+    """Adds a bounding box to an image.
+    Bounding box coordinates can be specified in either absolute (pixel) or
+    normalized coordinates by setting the use_normalized_coordinates argument.
+    Each string in display_str_list is displayed on a separate line above the
+    bounding box in black text on a rectangle filled with the input 'color'.
+    If the top of the bounding box extends to the edge of the image, the strings
+    are displayed below the bounding box.
+    Args:
+      image: a PIL.Image object.
+      ymin: ymin of bounding box.
+      xmin: xmin of bounding box.
+      ymax: ymax of bounding box.
+      xmax: xmax of bounding box.
+      color: color to draw bounding box. Default is red.
+      thickness: line thickness. Default value is 4.
+      display_str_list: list of strings to display in box
+                        (each to be shown on its own line).
+      use_normalized_coordinates: If True (default), treat coordinates
+        ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
+        coordinates as absolute.
+    """
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+    if use_normalized_coordinates:
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width, ymin * im_height, ymax * im_height)
+    else:
+        (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
+        
+    draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)], width=thickness, fill=color)
+    try:
+        font = ImageFont.truetype('arial.ttf', 24)
+    except IOError:
+        font = ImageFont.load_default()
+
+    # If the total height of the display strings added to the top of the bounding
+    # box exceeds the top of the image, stack the strings below the bounding box
+    # instead of above.
+    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+    # Each display_str has a top and bottom margin of 0.05x.
+    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = bottom + total_display_str_height
+    # Reverse list and print from bottom to top.
+    for display_str in display_str_list[::-1]:
+        text_width, text_height = font.getsize(display_str)
+        margin = np.ceil(0.05 * text_height)
+        draw.rectangle([(left, text_bottom - text_height - 2 * margin), (left + text_width, text_bottom)], fill=color)
+        draw.text((left + margin, text_bottom - text_height - margin), display_str, fill='black', font=font)
+        text_bottom -= text_height - 2 * margin
+
 
 def visualize_boxes_and_labels_on_image_array(
         image,
         boxes,
         classes,
         scores,
-        category_index,
+        #category_index,
         instance_masks=None,
         instance_boundaries=None,
         keypoints=None,
@@ -223,4 +323,241 @@ def visualize_boxes_and_labels_on_image_array(
             )
 
     return image
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------------- New
+
+
+
+
+
+
+def get_bounding_box_for_image_array(
+        image,
+        ymin,
+        xmin,
+        ymax,
+        xmax,
+        color='red',
+        thickness=4,
+        display_str_list=(),
+        use_normalized_coordinates=True):
+    """Adds a bounding box to an image (numpy array).
+    Bounding box coordinates can be specified in either absolute (pixel) or
+    normalized coordinates by setting the use_normalized_coordinates argument.
+    Args:
+    image: a numpy array with shape [height, width, 3].
+    ymin: ymin of bounding box.
+    xmin: xmin of bounding box.
+    ymax: ymax of bounding box.
+    xmax: xmax of bounding box.
+    color: color to draw bounding box. Default is red.
+    thickness: line thickness. Default value is 4.
+    display_str_list: list of strings to display in box
+    (each to be shown on its own line).
+    use_normalized_coordinates: If True (default), treat coordinates
+    ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
+    coordinates as absolute.
+    """
+    image_pil = Image.fromarray(np.uint8(image)).convert('RGB')
+    # draw_bounding_box_on_image(image_pil, ymin, xmin, ymax, xmax, color,
+    #                            thickness, display_str_list,
+    #                            use_normalized_coordinates)
+    # np.copyto(image, np.array(image_pil))
+
+    left, top, right, bottom = get_bounding_box_for_image(image_pil, ymin, xmin, ymax, xmax, color,
+                               thickness, display_str_list,
+                               use_normalized_coordinates)
+
+    return (left, top, right, bottom)
+
+
+def get_bounding_box_for_image(
+        image,
+        ymin,
+        xmin,
+        ymax,
+        xmax,
+        color='red',
+        thickness=4,
+        display_str_list=(),
+        use_normalized_coordinates=True):
+    """Adds a bounding box to an image.
+    Bounding box coordinates can be specified in either absolute (pixel) or
+    normalized coordinates by setting the use_normalized_coordinates argument.
+    Each string in display_str_list is displayed on a separate line above the
+    bounding box in black text on a rectangle filled with the input 'color'.
+    If the top of the bounding box extends to the edge of the image, the strings
+    are displayed below the bounding box.
+    Args:
+      image: a PIL.Image object.
+      ymin: ymin of bounding box.
+      xmin: xmin of bounding box.
+      ymax: ymax of bounding box.
+      xmax: xmax of bounding box.
+      color: color to draw bounding box. Default is red.
+      thickness: line thickness. Default value is 4.
+      display_str_list: list of strings to display in box
+                        (each to be shown on its own line).
+      use_normalized_coordinates: If True (default), treat coordinates
+        ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
+        coordinates as absolute.
+    """
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+    if use_normalized_coordinates:
+        (left, top, right, bottom) = (xmin * im_width, ymin * im_height, xmax * im_width, ymax * im_height)
+    else:
+        (left, top, right, bottom) = (xmin, ymin, xmax, ymax)
+
+    
+    # draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)], width=thickness, fill=color)
+    # try:
+    #     font = ImageFont.truetype('arial.ttf', 24)
+    # except IOError:
+    #     font = ImageFont.load_default()
+
+    # # If the total height of the display strings added to the top of the bounding
+    # # box exceeds the top of the image, stack the strings below the bounding box
+    # # instead of above.
+    # display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+    # # Each display_str has a top and bottom margin of 0.05x.
+    # total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+    # if top > total_display_str_height:
+    #     text_bottom = top
+    # else:
+    #     text_bottom = bottom + total_display_str_height
+    # # Reverse list and print from bottom to top.
+    # for display_str in display_str_list[::-1]:
+    #     text_width, text_height = font.getsize(display_str)
+    #     margin = np.ceil(0.05 * text_height)
+    #     draw.rectangle([(left, text_bottom - text_height - 2 * margin), (left + text_width, text_bottom)], fill=color)
+    #     draw.text((left + margin, text_bottom - text_height - margin), display_str, fill='black', font=font)
+    #     text_bottom -= text_height - 2 * margin
+
+    return (int(left), int(top), int(right), int(bottom))
+
+
+def get_bounding_boxes_for_image_array(
+        image,
+        boxes,
+        classes,
+        scores,
+        #category_index,
+        instance_masks=None,
+        instance_boundaries=None,
+        keypoints=None,
+        use_normalized_coordinates=False,
+        max_boxes_to_draw=None,
+        min_score_thresh=.5,
+        agnostic_mode=False,
+        line_thickness=4,
+        groundtruth_box_visualization_color='black',
+        skip_scores=True,
+        skip_labels=True):
+    """Overlay labeled boxes on an image with formatted scores and label names.
+    This function groups boxes that correspond to the same location
+    and creates a display string for each detection and overlays these
+    on the image. Note that this function modifies the image in place, and returns
+    that same image.
+    Args:
+      image: uint8 numpy array with shape (img_height, img_width, 3)
+      boxes: a numpy array of shape [N, 4]
+      classes: a numpy array of shape [N]. Note that class indices are 1-based,
+        and match the keys in the label map.
+      scores: a numpy array of shape [N] or None.  If scores=None, then
+        this function assumes that the boxes to be plotted are groundtruth
+        boxes and plot all boxes as black with no classes or scores.
+      category_index: a dict containing category dictionaries (each holding
+        category index `id` and category name `name`) keyed by category indices.
+      instance_masks: a numpy array of shape [N, image_height, image_width] with
+        values ranging between 0 and 1, can be None.
+      instance_boundaries: a numpy array of shape [N, image_height, image_width]
+        with values ranging between 0 and 1, can be None.
+      keypoints: a numpy array of shape [N, num_keypoints, 2], can
+        be None
+      use_normalized_coordinates: whether boxes is to be interpreted as
+        normalized coordinates or not.
+      max_boxes_to_draw: maximum number of boxes to visualize.  If None, draw
+        all boxes.
+      min_score_thresh: minimum score threshold for a box to be visualized
+      agnostic_mode: boolean (default: False) controlling whether to evaluate in
+        class-agnostic mode or not.  This mode will display scores but ignore
+        classes.
+      line_thickness: integer (default: 4) controlling line width of the boxes.
+      groundtruth_box_visualization_color: box color for visualizing groundtruth
+        boxes
+      skip_scores: whether to skip score when drawing a single detection
+      skip_labels: whether to skip label when drawing a single detection
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3) with overlaid boxes.
+    """
+    # Create a display string (and color) for every box location, group any boxes
+    # that correspond to the same location.
+    box_to_display_str_map = collections.defaultdict(list)
+    box_to_color_map = collections.defaultdict(str)
+    box_to_instance_masks_map = {}
+    box_to_instance_boundaries_map = {}
+    box_to_keypoints_map = collections.defaultdict(list)
+    if not max_boxes_to_draw:
+        max_boxes_to_draw = boxes.shape[0]
+    for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+        if scores is None or scores[i] > min_score_thresh:
+            box = tuple(boxes[i].tolist())
+            if instance_masks is not None:
+                box_to_instance_masks_map[box] = instance_masks[i]
+            if instance_boundaries is not None:
+                box_to_instance_boundaries_map[box] = instance_boundaries[i]
+            if keypoints is not None:
+                box_to_keypoints_map[box].extend(keypoints[i])
+            if scores is None:
+                box_to_color_map[box] = groundtruth_box_visualization_color
+            else:
+                display_str = ''
+                if not skip_labels:
+                    if not agnostic_mode:
+                        if classes[i] in category_index.keys():
+                            class_name = category_index[classes[i]]['name']
+                        else:
+                            class_name = 'N/A'
+                        display_str = str(class_name)
+                if not skip_scores:
+                    if not display_str:
+                        display_str = '{}%'.format(int(100*scores[i]))
+                    else:
+                        display_str = '{}: {}%'.format(display_str, int(100*scores[i]))
+                box_to_display_str_map[box].append(display_str)
+                if agnostic_mode:
+                    box_to_color_map[box] = 'DarkOrange'
+                else:
+                    box_to_color_map[box] = STANDARD_COLORS[classes[i] % len(STANDARD_COLORS)]
+
+    bounding_boxes = []
+    # Draw all boxes onto image.
+    for box, color in box_to_color_map.items():
+        ymin, xmin, ymax, xmax = box
+        left, top, right, bottom = get_bounding_box_for_image_array(
+            image,
+            ymin,
+            xmin,
+            ymax,
+            xmax,
+            color=color,
+            thickness=line_thickness,
+            display_str_list=box_to_display_str_map[box],
+            use_normalized_coordinates=use_normalized_coordinates
+        )
+
+        bounding_boxes.append((left, top, right, bottom))
+
+    return bounding_boxes
 
