55dad203dce57d58a4d0997f596461fedc990788
David Thompson <00hello@users.noreply.github.com>
2018-09-18 16:43:36 -0400

    Fixed problem with queues filling and blocking by using two sharedprocess queues and one thread-bound priority queue just for tracking loop

--- multi_object_tracking.py
+++ multi_object_tracking.py
@@ -27,6 +27,7 @@ import sys
 from threading import Thread
 import detection_visualization_util
 from random import randint
+import concurrent.futures
 
 # Use a service account
 cred = credentials.Certificate(os.environ['FIREBASE_CREDENTIALS'])
@@ -71,11 +72,35 @@ s3_bucket = s3_res.Bucket('grassland-images')
 
 lambda_url = os.environ['LAMBDA_DETECTION_URL']
 
+o_queue_max = 80
+p_queue_max = 300
+
+i_queue = Queue() # input queue    
+o_queue = Queue(maxsize=o_queue_max) # output queue
+p_queue = PriorityQueue(maxsize=p_queue_max) # priority queue
+
+def delete_from_s3(s3_bucket, file_name_ext):
+    try:
+        s3_bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': file_name_ext
+                    }
+                ]
+            }
+        )
+    except:
+        import traceback
+        traceback.print_exc()
+
+
+                
 def get_detections_error_callback(the_exception):
     print("get_detections_error_callback called")
     print(the_exception)
 
-def get_detections(frame_number,frame):
+def get_detections(frame_number, frame, no_callback=False):    
     try:
         image = Image.fromarray(frame) # Remember Opencv images are in 'BGR'
         file_name_ext = 'frame_'+str(frame_number)+'.jpg'
@@ -98,7 +123,6 @@ def get_detections(frame_number,frame):
 
         response_dict = json.loads(response.text)
 
-
         output_dict = response_dict['prediction_result']
 
         detection_boxes = np.array(output_dict['detection_boxes'])
@@ -109,18 +133,39 @@ def get_detections(frame_number,frame):
         output_dict['detection_classes'] = detection_classes
 
 
-        return ( frame_number, {"detected": 1, "frame": frame, "output_dict": output_dict} )
+        detected_frame_tuple = ( frame_number, {"detected": 1, "frame": frame, "output_dict": output_dict} )
+        if no_callback:
+            add_to_o_queue(detected_frame_tuple)
+            delete_from_s3(s3_bucket, file_name_ext)
+        else:
+            delete_from_s3(s3_bucket, file_name_ext)
+            return detected_frame_tuple
+
+
+    except KeyboardInterrupt:
+        import traceback
+        traceback.print_exc()
+        raise                
     except:
         import traceback
         traceback.print_exc()
         
-        raise # Without this, the regular callback of apply_async will be called
+        #raise # Without this raise, the regular callback of apply_async will be called
 
         
-def add_to_p_queue(detected_frame_tuple):
+def add_to_o_queue(detected_frame_tuple):
     try:
-        print("Adding detection to p_queue")
-        p_queue.put(detected_frame_tuple)
+        if o_queue.full():
+            print("o_queue FULL")
+            print("-------PROGRAM STOPPED UNTIL o_queue DRAINED INTO p_queue-----")
+            
+        #if not o_queue.full():
+        #    print("Adding detection to o_queue")
+        #    print("o_queue size")
+        #    print(o_queue.qsize())
+        o_queue.put(detected_frame_tuple)
+        #else:
+        #    print("o_queue full")
     except:
         import traceback
         traceback.print_exc()
@@ -128,16 +173,21 @@ def add_to_p_queue(detected_frame_tuple):
         
 
 
+
 # if a video path was not supplied, grab the reference to the web cam
 if not args.get("video", False):
-    print("[INFO] starting video stream...")
-    vs = VideoStream(usePiCamera=args["picamera"] > 0).start() # Default to PiCamera
+    framerate = 15
+        
+    print("[INFO] starting video stream...")    
+    vs = VideoStream(usePiCamera=args["picamera"], resolution=(800, 464), framerate=framerate).start() # Default to PiCamera
+    print("[INFO] Warming up camera...")
+    time.sleep(3)
     
- 
-# otherwise, grab a reference to the video file
+    # otherwise, grab a reference to the video file
 else:
+    framerate = 30
     #vs = cv2.VideoCapture(args["video"])
-    vs = FileVideoStream(args["video"], queueSize=30).start()
+    vs = FileVideoStream(args["video"], queueSize=15).start()
 
     # loop over frames from the video stream
 
@@ -153,7 +203,7 @@ if args["display"] == 1:
 else:
     display = False
     
-run_worker = False
+run_tracking_loop = False
 
 count_read_frame = 0
 count_write_frame = 1
@@ -176,26 +226,63 @@ print("Waiting "+str(lambda_wakeup_duration)+" seconds for function to wake up..
 time.sleep(lambda_wakeup_duration)
 
 main_fps = FPS().start()
-worker_fps = FPS().start()
 
-def worker():
-    global p_queue
-    global worker_fps
+
+def tracking_loop():
     try:
+        print("STARTING TRACKING_LOOP")
+        #global tracking_loop_fps
+        tracking_loop_fps = FPS().start()
+        
         frame_loop_count = 0
-        while run_worker:
+        
+        #while run_tracking_loop:
+        while True:
             # Pull first/next frame tuple from p_queue
             #queue_get_start_time = time.time()
+            
             try:
-                frame_number, frame_dict = p_queue.get(timeout=20)
+                if p_queue.full():
+                    print("p_queue FULL .......................................................")
+                    # Then we'll need to drop the next frame from the stack
+                    frame_number, frame_dict = p_queue.get()
+
+                    frame_loop_count = frame_number + 1 # Since we're skipping a frame, we'll need to make sure frame_loop_count catches up to the next frame_number 
+                    continue # Start loop again
+                else:
+                    
+                    p_queue.put(o_queue.get(timeout=20)) # Bring frames from shared output queue into priority queue so they can be used in order
+                    frame_number, frame_dict = p_queue.get(timeout=20)
+                    
             except:
                 # If there are no more frames being put into p_queue, stop the thread
                 print("No More Frames In p_queue")
+                tracking_loop_fps.stop()
+                print("[INFO] approx. Tracking_Loop FPS: {:.2f}".format(tracking_loop_fps.fps()))
                 return
             
             #print("queue_get_start_time Time:", time.time()-queue_get_start_time)
 
+            try:
+                if int((datetime.now() - running_notice_since).total_seconds()) > 40:
+                    running_notice_since = datetime.now()
+                    print("tracking_loop still running ...")
+            except:
+                running_notice_since = datetime.now()
+
+
+            # if p_queue.qsize() > 70:
+            #     print("p_queue size")
+            #     print(p_queue.qsize())
+
             if frame_number > frame_loop_count:
+                try:
+                    if int((datetime.now() - frame_number_high_notice_since).total_seconds()) > 10:
+                        frame_number_high_notice_since = datetime.now()
+                        print("frame number too high ...")
+                except:
+                    frame_number_high_notice_since = datetime.now()
+
                 p_queue.task_done() 
                 '''
                 This tracking loop has outpaced our actual frames because of our "detections" bottleneck
@@ -221,7 +308,7 @@ def worker():
 
                     output_dict = frame_dict.get("output_dict")
                     
-
+                    print("DETECTIONS RECEIVED")
                     tracker_boxes = detection_visualization_util.get_bounding_boxes_for_image_array(
                         this_frame,
                         output_dict['detection_boxes'],
@@ -270,24 +357,27 @@ def worker():
                     # -> if frame_dict.get("detected") == 1:
                     
                 else:
-                    
-                    # grab the updated bounding box coordinates (if any) for each
-                    # object that is being tracked
-
-                    #ok, bbox = tracker.update(this_frame)
-                    ok, tracker_boxes = trackers.update(this_frame)
 
                     
-                    if not ok:
-                        # Tracking failure
-                        #print("----- OBJECT TRACKER NOT UPDATING !! -----")
-                        if display:
-                            cv2.putText(this_frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
-                    else:
-                        # Tracking success
-                        if manual:
-                            tracker_boxes = []
-                            tracker_boxes.append(bbox)
+
+                    if frame_number % (framerate - 14) == 0:
+                        # grab the updated bounding box coordinates (if any) for each
+                        # object that is being tracked
+
+                        #ok, bbox = tracker.update(this_frame)
+                        ok, tracker_boxes = trackers.update(this_frame)
+
+
+                        if not ok:
+                            # Tracking failure
+                            #print("----- OBJECT TRACKER NOT UPDATING !! -----")
+                            if display:
+                                cv2.putText(this_frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+                        else:
+                            # Tracking success
+                            if manual:
+                                tracker_boxes = []
+                                tracker_boxes.append(bbox)
                         
                     
                     # -> if frame_dict.get("detected") == 1: else:
@@ -327,7 +417,7 @@ def worker():
                         break
 
 
-                worker_fps.update()
+                tracking_loop_fps.update()
 
                 # Let the system know the task is done
                 p_queue.task_done()
@@ -345,13 +435,65 @@ def worker():
         raise
 
 
+def detection_loop():
+    try:
+        print("STARTING DETECTION_LOOP")
+        while True:
 
-p_queue = PriorityQueue()
-pool = Pool(multiprocessing.cpu_count())
+            try:
+                if not i_queue.empty():
 
+                    frame_number, frame = i_queue.get(block=False)
+                    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+                        #if not o_queue.full(): # If o_queue is full the tracker isn't moving fast enough and to avoid memory problems, we'll just drop frames
+                        #before_executor = time.time()
+                        executor.submit(get_detections, frame_number, frame, no_callback=True)
+                        #print("Executor Time:", time.time()-before_executor)
+
+                else:
+                    try:
+                        if int((datetime.now() - idle_since).total_seconds()) > 40:
+                            idle_since = datetime.now()
+                            print("no i_queue items for detection loop .................")
+                    except:
+                        idle_since = datetime.now()
+
+                        
+
+            except Empty:
+                print("i_queue empty error")                        
+            except KeyboardInterrupt:
+                import traceback
+                traceback.print_exc()
+                raise                
+            except:
+                import traceback
+                traceback.print_exc()
+
+    except KeyboardInterrupt:
+        import traceback
+        traceback.print_exc()
+        raise                
+    except:
+        import traceback
+        traceback.print_exc()
+
+
+
+def o_queue_exceeds_safe_threshold():
+    return o_queue.qsize() > int(o_queue_max *0.9)
+
+    
+dl = multiprocessing.Process(target=detection_loop)
+dl.daemon = True
+dl.start()
+#pool = Pool(1, detection_loop)
+#pool.apply_async(func=detection_loop, args=())
+
+run_feed_loop = True
 try:
-    while True:
-
+    while run_feed_loop:
+        
         # grab the current frame, then handle if we are using a
         # VideoStream or VideoCapture object
         if args.get("video", False):
@@ -361,7 +503,7 @@ try:
                 frame = vs.read()
         else:
             frame = vs.read()
-            
+
 
         # check to see if we have reached the end of the stream
         if frame is None:
@@ -386,52 +528,79 @@ try:
 
 
 
-            current_main_fps = main_fps._numFrames / (datetime.now() - main_fps._start).total_seconds()
-
-            main_fps_divisor = int(current_main_fps * 4)
-            if main_fps_divisor == 0:
-                main_fps_divisor = 1
-
-            if main_fps._numFrames == 0 or main_fps._numFrames % main_fps_divisor == 0:
-                # Run asynchronous object detection on frame
+            # current_main_fps = main_fps._numFrames / (datetime.now() - main_fps._start).total_seconds()
+            # main_fps_divisor = int(current_main_fps * 4)
+            # if main_fps_divisor == 0:
+            #     main_fps_divisor = 1
+            #if main_fps._numFrames == 0 or main_fps._numFrames % main_fps_divisor == 0:
 
 
-                #print("about to call apply_async")
-                pool.apply_async(func=get_detections, args=(main_fps._numFrames, frame), callback=add_to_p_queue, error_callback=get_detections_error_callback)
+            if main_fps._numFrames == 0 or (datetime.now() - last_i_queue_put).total_seconds() > 4:
+                
+             
+                # Put frame in i_queue to wait for asynchronous object detection
+                if not o_queue_exceeds_safe_threshold(): # Since all extant i_queue frames eventually go into o_queue and if o_queue exceeds maxsize, program will stop
+                    #print("Putting frame in i_queue")
+                    i_queue.put((main_fps._numFrames, frame))
+                    
+                    if main_fps._numFrames == 0:
+                        first_frame_detected = True
 
-                #print("called apply_async")
+                    main_fps.update()
+                else:
+                    try:
+                        if int((datetime.now() - i_queue_notice_since).total_seconds()) > 40:
+                            i_queue_notice_since = datetime.now()
+                            print("Cannot put frame in i_queue. o_queue 90% full")
+                    except:
+                        i_queue_notice_since = datetime.now()
 
-                print("p_queue size")
-                print(p_queue.qsize())
+                    
 
-                if main_fps._numFrames == 0:
-                    first_frame_detected = True
+                # print("i_queue size")
+                # print(i_queue.qsize())
 
+                last_i_queue_put = datetime.now()
+
+                # if i_queue.qsize() > 10:
+                #     print("i_queue size too large. Starting detection_loop again")
+                #     dl.run()
+
+
+                #import pdb; pdb.set_trace()
             else:
-                # store frame in priority queue
-                p_queue.put((main_fps._numFrames, {"detected": 0, "frame": frame}))
+                if not o_queue_exceeds_safe_threshold(): # Skipping when it's 90% full. The remaining 10% is given to detected frames
+                    # store frame in output queue
+                    o_queue.put((main_fps._numFrames, {"detected": 0, "frame": frame}))
+                    main_fps.update()
+                else:
 
+                    try:
+                        if int((datetime.now() - printout_since).total_seconds()) > 30:
+                            printout_since = datetime.now()
+                            print("o_queue 90% full")
+                            print("Can't add new frames to o_queue")
+                    except:
+                        printout_since = datetime.now()
 
+                    
+                    
 
-            # update the FPS counter
-            main_fps.update()
-
-
-            
             # -> if frame is not None and not np.array_equal(last_frame, frame):
-        
 
-        
-        
-        if first_frame_detected and not run_worker:
-            print("about to start")
-            run_worker = True
 
-            t = Thread(target=worker, args=())
-            t.daemon = True
-            t.start()
 
-            #pool.apply_async(worker) # Doesn't work. Program freezes
+
+        if first_frame_detected and not run_tracking_loop:
+            run_tracking_loop = True
+
+            # t = Thread(target=tracking_loop, args=())
+            # t.daemon = True
+            # t.start()
+            #tracking_pool = Pool(1, tracking_loop)
+            
+            tl = multiprocessing.Process(target=tracking_loop)
+            tl.start()
 
 
 
@@ -444,16 +613,25 @@ except:
     import traceback
     traceback.print_exc()
     
-finally:    
+finally:
 
-    print("Finishing up items in p_queue")
-    p_queue.join()
-    print("CLOSING POOL")
-    pool.close()
-    print("TERMINATING POOL")
-    pool.terminate()
+    # print("FINISHING UP ITEMS IN p_queue")
+    # p_queue.join() # Only works if tracking_loop runs under same process as this
 
-    pool.join()
+    print("TERMINATING detection_loop")
+    dl.terminate()
+    print("TERMINATING tracking_loop")
+    tl.terminate()
+    
+    #print("CLOSING POOL")
+    #pool.close()
+    #tracking_pool.close()
+    #print("TERMINATING POOL")
+    #pool.terminate()
+    #tracking_pool.terminate()
+
+    #pool.join()
+    #tracking_pool.join()
 
     vs.stop()
     # if we are using a webcam, release the pointer
@@ -470,8 +648,8 @@ finally:
 
     # stop the timer and display FPS information
     main_fps.stop()
-    worker_fps.stop()
+    
     print("[INFO] elasped time: {:.2f}".format(main_fps.elapsed()))
     print("[INFO] approx. Main FPS: {:.2f}".format(main_fps.fps()))
-    print("[INFO] approx. Worker FPS: {:.2f}".format(worker_fps.fps()))
+    #print("[INFO] approx. Tracking_Loop FPS: {:.2f}".format(tracking_loop_fps.fps()))
 
