93c900af460bdca18bf2ec5a0a91943e27e7ab04
00hello <00hello@users.noreply.github.com>
2019-03-08 11:58:55 -0500

    Major Version Change: Switched to LevelDB for database, S2 Geometry for geospatial indexing, and Deck.gl for both calibrating the node and as a GUI for the data. Notes to follow

---
+++ gui/src/server/index.js
@@ -0,0 +1,153 @@
+#!/usr/bin/env node
+'use strict';
+
+const net = require('net');
+const express = require('express')
+
+// const app = express()
+const port = 8080
+
+var expressWs = require('express-ws')
+var expressWs = expressWs(express());
+var app = expressWs.app;
+
+
+var calibration;
+
+
+
+app.use(express.static('dist'))
+
+// Use this server to send calibration values received from map/browser down to node and ...
+// ... send frame dimensions (frame_dim) received from node up to map/browser
+app.ws('/send_calibration', (ws, req) => {
+	// console.error('websocket connection from browser');
+	// for (var t = 0; t < 3; t++)
+	//   setTimeout(() => ws.send('Map, the server received your message', ()=>{}), 1000*t);
+
+	ws.on('message', function incoming(message) {
+		
+		// console.log('Server received calibration from map');
+		
+		// Setting 'calibration' variable to calibration value
+		calibration = message;
+
+		// Create new socket and connect to node
+		var client = new net.Socket();
+		client.setTimeout(4000);
+		client = client.setEncoding('utf8');
+		client.connect(8765, '127.0.0.1', function() {
+			// console.log('Connected to node calibration socket server');
+
+			// Send calibration to node's websocket/socket server
+			var if_sent = client.write(calibration);
+			if (if_sent) {
+				// console.log("Calibration sent");
+			}
+		});
+
+		
+		let chunks = []; // https://stackoverflow.com/a/45030133/8941739			
+		client.on('data', function(data) {
+			chunks.push(data); // https://stackoverflow.com/a/45030133/8941739
+		}).on('end', function() {
+			if (chunks.length > 0) {
+				// console.log("Received frame dimensions (frame_dim) ...");
+				let this_data = chunks.join("");
+				var parsed_data = JSON.parse(this_data.replace(/\'/g, '"'));
+				// console.log(parsed_data);
+				// Send frame_dim to browser
+				ws.send(JSON.stringify(parsed_data));
+			}
+			client.destroy(); // kill client after server's response
+		});
+
+		client.on('error', function(ex) {
+			// console.log("Something happened trying to send calibration to node ");
+			client.destroy(); // kill client after error
+			// console.log(ex);
+		});
+
+		client.on('timeout', function() {
+			// console.log('socket timeout');
+			client.end();
+		});
+
+
+	});
+});
+
+
+
+
+
+// When browser requests tracklets, get them from node and return them
+// Browser requests tracklets from server. Server gets them from Node. Server sends tracklets to browser
+app.ws('/get_tracklets', (ws, req) => {
+
+	// console.log("called get_tracklets");
+
+	ws.on('message', function incoming(message) {
+		
+		// console.log('Server received get_tracklets from map');
+
+		var client = new net.Socket();
+		client.setTimeout(4000);
+		client = client.setEncoding('utf8');
+		client.connect(8766, '127.0.0.1', function() {
+			// console.log('Connected to tracklets');
+			// console.log("message");
+			//console.log(message);
+			client.write(message);
+		});
+
+		let chunks = []; // https://stackoverflow.com/a/45030133/8941739			
+		client.on('data', function(data) {
+			chunks.push(data); // https://stackoverflow.com/a/45030133/8941739
+		}).on('end', function() {
+
+			//console.log(chunks);
+			if (chunks.length > 0) {
+				let this_data = chunks.join("");
+				var trackableObjectsArray = JSON.parse(this_data.replace(/'/g, '"'));
+
+				if (typeof trackableObjectsArray !== 'undefined') {
+					try {
+
+						// console.log("-------------------------------------------------------");
+						// console.log(JSON.stringify(trackableObjectsArray));
+						// Send tracklets to browser
+						ws.send(JSON.stringify(trackableObjectsArray));
+
+					} catch(e) {
+						console.log("error", e);
+					}
+				} else {
+					// Send empty array to browser 
+					ws.send(JSON.stringify([]));
+				}
+			} else {
+				// Send empty array to browser
+				ws.send(JSON.stringify([]));
+			}
+			
+			client.destroy(); // kill client after server's response
+		});
+
+		client.on('error', function(ex) {
+			console.log("Something happened trying to get trackelets from node ");
+			client.destroy(); // kill client after error
+			console.log(ex);
+		});
+
+		client.on('timeout', function() {
+			console.log('socket timeout');
+			client.end();
+		});
+		
+	});
+});
+
+
+	
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- pyimagesearch/trackableobject.py
+++ pyimagesearch/trackableobject.py
@@ -1,11 +1,72 @@
 class TrackableObject:
-    def __init__(self, objectID, centroid, boxoid):
+    def __init__(self, objectID, centroid_frame_timestamp, detection_class_id, centroid, boxoid, bbox_rw_coords):
         # store the object ID, then initialize a list of centroids
         # using the current centroid
         self.objectID = objectID
-        self.centroids = [centroid]
-        self.boxoids = [boxoid]
+
+        # initialize instance variable, 'oids' as a list
+        self.oids = []
+        
+        # initialize instance variable, 'centroids' as a list
+        self.centroids = []
+
+        # initialize instance variable, 'boxoids' as a list
+        self.boxoids = []
+
+        # initialize instance variable, 'bbox_rw_coords' as a list
+        self.bbox_rw_coords = []
+
+        # initialize instance variable 'detection_class_id' as 0
+        self.detection_class_id = detection_class_id
 
         # initialize a boolean used to indicate if the object has
         # already been counted or not
         self.counted = False
+
+        # initialize a boolean used to indicate if the object has left the node's field of view and the tracks complete
+        self.complete = False
+
+        # pass first boxoid to 'append_boxoids' method for processing
+        self.append_boxoid(boxoid)
+
+        # pass first centroid to 'append_centroids' method for processing
+        self.append_centroid(centroid)
+
+
+        self.append_oids(centroid_frame_timestamp, detection_class_id, centroid, boxoid, bbox_rw_coords)
+
+
+
+    def append_centroid(self, centroid):
+        pass
+        #self.centroids.append(list(centroid))
+        
+        
+    def append_boxoid(self, boxoid):
+
+        #self.boxoids.append(list(boxoid))
+
+        # if self.detection_class_id > 0 and boxoid[5] <= 0: # if object's class has been identified already but this isn't a new identification
+        #     pass # ... then don't change the current detection class. Even if the new detection_class_id is a -1, which means that the detection has changed but we'll stick with the first detected object class
+        # else: # if the object's class hasn't been identified yet or this is a new identification from a detected frame or a -1
+        #     self.detection_class_id = boxoid[5]
+
+        pass
+    
+
+    def append_oids(self, centroid_frame_timestamp, detection_class_id, centroid, boxoid, bbox_rw_coords):
+
+        if self.detection_class_id > 0 and detection_class_id <= 0: # if object's class has been identified already but this isn't a new identification
+            pass # ... then don't change the current detection class. Even if the new detection_class_id is a -1, which means that the detection has changed but we'll stick with the first detected object class
+        else: # if the object's class hasn't been identified yet or this is a new identification from a detected frame or a -1
+            self.detection_class_id = detection_class_id
+
+
+        oid = {
+            "frame_timestamp": centroid_frame_timestamp,
+            "centroid": list(centroid),
+            "boxoid": list(boxoid),
+            "bbox_rw_coords": bbox_rw_coords
+        }
+        
+        self.oids.append(oid)
